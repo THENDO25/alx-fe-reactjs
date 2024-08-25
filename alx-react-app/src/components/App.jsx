190997fc4b078d07a27510cf9192348865b1a7ef
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 21:44:07 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -15,13 +15,14 @@ function App() {
 
   return (
     <>
+      
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
       <Header />
       <WelcomeMessage />
       <MainContent/>
       <Footer/>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
       <a href="https://react.dev" target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
