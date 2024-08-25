77bccf424583490bf91a742cee6802e283abdbc0
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 21:56:53 +0200

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,9 +1,11 @@
-function Footer() {
-    return (
-        <footer>
-        <p>© 2023 City Lovers</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
