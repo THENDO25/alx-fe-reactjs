98fb7572a560b777deef208607fb4a59ddf0e5d6
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 21:37:34 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -13,15 +13,16 @@ function App() {
 
   return (
     <>
-     
-        
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Header />
+      <WelcomeMessage />
+      <MainContent/>
+      <Footer/>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -32,11 +33,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-         </p>
-      <WelcomeMessage />
-      <MainContent/>
-      <Footer/>
-      <Header/>
+      </p>
       <UserProfile name="Alice" age="25" bio="I do Hard things,Loves gaming and reading" />
     </>
   )
