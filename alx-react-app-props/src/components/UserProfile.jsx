e8d294b736a0cc1eed6e431fef4e1fa6aa23e490
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 23:12:19 +0200

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../UserContext';
 
-const UserProfile = (props) => {
+function UserProfile() {
+  const { name, age, bio } = useContext(UserContext);
+
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{age}</span></p>
+      <p>Bio: {bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
