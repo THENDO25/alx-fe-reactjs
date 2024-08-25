9afe9127262361bd07e107d3c20822a77c9fef0e
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 23:10:00 +0200

    Update and rename UserContent.js to UserContext.js

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,20 @@
+
+import { createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    name: 'John Doe',
+    age: 30,
+    bio: 'Software developer',
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
