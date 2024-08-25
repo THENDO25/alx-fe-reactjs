72c4329b4ded053095d6946e603a214103313dd3
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 22:13:35 +0200

    Update Header.jsx

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-const Header = () => {
-  return (
-    <header>
-      <h1>My Favorite Cities</h1>
-    </header>
-  );
-};
+function Header() {
+    return (
+        <header>
+            <h1>My Favorite Cities</h1>
+        </header>
+    );
+}
 
 export default Header;
