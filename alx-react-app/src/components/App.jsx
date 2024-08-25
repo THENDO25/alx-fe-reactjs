a184fd5163494920e7f3780f27a5799e473e929a
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 21:30:50 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -38,7 +38,13 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+         <div>
+      <WelcomeMessage />
+      <MainContent/>
+      <Footer/>
+         <Header/>
+<UserProfile name="Alice" age="25" bio="I do Hard things,Loves gaming and reading" />
+        </div>
       </p>
     </>
   )
