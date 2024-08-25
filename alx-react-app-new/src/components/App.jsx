cfd5d5507ce0013d4e550dfc1f5acdc028220943
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 22:53:54 +0200

    Update App.jsx

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import React from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg'; // Fix: Change /vite.svg to ./vite.svg
+import viteLogo from './vite.svg';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import Header from './Header';
