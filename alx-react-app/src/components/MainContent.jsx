af59a7768215993df32bad3b9c6a0fa0ab562e9e
THENDO25 <thendomukwevho1125@gmail.com>
2024-08-25 22:14:08 +0200

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-const MainContent = () => {
-  return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-};
+function MainContent() {
+    return (
+        <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
 
 export default MainContent;
