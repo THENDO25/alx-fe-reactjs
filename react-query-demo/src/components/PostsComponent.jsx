74f6bd5bab47e41051103d6b2cc42b5b2d06a5aa
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 21:51:01 +0200

    added files

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return response.json();
+};
 
 const PostsComponent = () => {
+  const { data, error, isLoading, isError } = useQuery('posts', fetchPosts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div>
       <h1>Posts</h1>
-      {"this is my alc query progtam"}
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
