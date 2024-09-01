233a4ec3f421c6b839fe7bfc57f4b43d30f046a8
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 23:57:16 +0200

    added files

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Buy milk', completed: false },
+    { id: 2, text: 'Walk the dog', completed: false },
+  ]);
+
   return (
-    <div>
-      <h1>Todo List</h1>
-      {/* Todo list items */}
-    </div>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.text}
+          </span>
+          <button onClick={() => setTodos(todos.map((t) => (t.id === todo.id ? { ...t, completed: !t.completed } : t)))}>
+            Toggle
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
