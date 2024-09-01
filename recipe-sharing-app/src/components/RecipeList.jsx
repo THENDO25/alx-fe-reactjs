10aa049826d83fc10430868138b7ef5d30acb9c9
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 21:02:01 +0200

    added files

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useRecipeStore } from '../recipeStore';
 
-const SearchBar = () => {
-  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+const RecipeList = () => {
+  const { recipes, addFavorite, removeFavorite, generateRecommendations } = useRecipeStore();
 
   return (
-    <input
-      type="text"
-      placeholder="Search recipes..."
-      onChange={(e) => setSearchTerm(e.target.value)}
-    />
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={index}>
+            {recipe.name}
+            <button onClick={() => addFavorite(recipe.id)}>Add to Favorites</button>
+            <button onClick={() => removeFavorite(recipe.id)}>Remove from Favorites</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={generateRecommendations}>Generate Recommendations</button>
+    </div>
   );
 };
 
-export default SearchBar;
+export default RecipeList;
