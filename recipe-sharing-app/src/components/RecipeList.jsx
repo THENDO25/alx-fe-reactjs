b29099a6618b3c0ffc76b62f114b99b66c4dec11
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 21:12:00 +0200

    added files

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const { recipes, addFavorite, removeFavorite, generateRecommendations } = useRecipeStore();
@@ -10,6 +11,7 @@ const RecipeList = () => {
       <ul>
         {recipes.map((recipe, index) => (
           <li key={index}>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.name}</Link>
             {recipe.name}
             <button onClick={() => addFavorite(recipe.id)}>Add to Favorites</button>
             <button onClick={() => removeFavorite(recipe.id)}>Remove from Favorites</button>
