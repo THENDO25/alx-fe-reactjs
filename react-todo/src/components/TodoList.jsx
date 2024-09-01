8572b083d521d922d959d8f7638d669469080206
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 23:55:19 +0200

    added files

---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {/* Todo list items */}
+    </div>
+  );
+};
+
+export default TodoList;
