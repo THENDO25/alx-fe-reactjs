69e19bd8068c84e38cb6192f9e40b4c9548d6b0a
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 21:05:13 +0200

    added files

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,11 +3,19 @@ import { create } from 'zustand';
 const useRecipeStore = create(set => ({
   recipes: [],
   favorites: [],
+  searchTerm: '',
   addRecipe: (recipe) => set(state => ({ recipes: [...state.recipes, recipe] })),
   setRecipes: (recipes) => set({ recipes }),
   addFavorite: (recipeId) => set(state => ({ favorites: [...state.favorites, recipeId] })),
   removeFavorite: (recipeId) => set(state => ({
     favorites: state.favorites.filter(id => id !== recipeId)
+  })),
+  setSearchTerm: (searchTerm) => set({ searchTerm }),
+  updateRecipe: (recipeId, updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe => recipe.id === recipeId ? updatedRecipe : recipe)
+  })),
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
   })),
   recommendations: [],
   generateRecommendations: () => set(state => {
