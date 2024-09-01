ec8ab3ffb14a4a2d528457b6e979a3eb5409553e
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 21:23:49 +0200

    added files

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import FormikForm from "./components/formikForm.js";
+import RegistrationForm from "./components/RegistrationForm.js";
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <FormikForm />
+      <RegistrationForm/>
     </>
   )
 }
