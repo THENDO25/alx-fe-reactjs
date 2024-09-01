e6accf2f8807a4f531e5ef636e6967ec444f2952
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-01 21:47:50 +0200

    added files

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostsComponent = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      {"this is my alc query progtam"}
+    </div>
+  );
+};
+
+export default PostsComponent;
