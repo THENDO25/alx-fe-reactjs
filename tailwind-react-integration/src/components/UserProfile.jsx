2b5674eff63a8f8c40fe800179d576ff10432e5d
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-15 19:24:52 +0200

    4th commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-md mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-36 md:h-36 mx-auto" />
-      <h1 className="text-lg sm:text-xl md:text-xl lg:text-2xl text-blue-800 my-4">John Doe</h1>
+    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 p-2 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-md mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" />
+      <h1 className="text-lg sm:text-xl md:text-xl lg:text-2xl text-blue-800 hover:text-blue-500 my-4 transition-colors duration-300 ease-in-out">John Doe</h1>
       <p className="text-sm sm:text-base md:text-base lg:text-lg text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
