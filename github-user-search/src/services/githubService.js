957826b14a3ae60f539263138f0a03c91e1707c1
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-30 16:26:00 +0200

    2nd

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -7,13 +7,14 @@ const githubApi = axios.create({
   },
 });
 
-const fetchUserData = async (username, location, repoCount, page = 1) => {
-    const query = [];
-    if (username) query.push(`login:${username}`);
-    if (location) query.push(`location:${location}`);
-    if (repoCount) query.push(`repos:>${repoCount}`);
-  
-    const response = await githubApi.get(`search/users?q=${query.join('+')}&page=${page}&per_page=10`);
-    return response.data;
-  };
+const fetchUserData = async (username, location, minRepos, page) => {
+  const query = [];
+  if (username) query.push(`login:${username}`);
+  if (location) query.push(`location:${location}`);
+  if (minRepos) query.push(`repos:>${minRepos}`);
+
+  const response = await githubApi.get(`search/users?q=${query.join('+')}&page=${page}&per_page=10`);
+  return response.data;
+};
+
 export { fetchUserData };
