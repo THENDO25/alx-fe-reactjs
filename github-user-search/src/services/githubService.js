b0b666c0ef926365c26747d762e5d87b0915f3b5
THENDO25 <thendomukwevho1125@gmail.com>
2024-09-30 16:29:33 +0200

    2nd

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,21 +1,12 @@
-import axios from 'axios';
-
-const githubApi = axios.create({
-  baseURL: 'https://api.github.com/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 const fetchUserData = async (username, location, minRepos, page) => {
-  const query = [];
-  if (username) query.push(`login:${username}`);
-  if (location) query.push(`location:${location}`);
-  if (minRepos) query.push(`repos:>${minRepos}`);
-
-  const url = `search/users?q=${query.join('+')}&page=${page}&per_page=10`;
-  const response = await githubApi.get(url);
-  return response.data;
-};
-
-export { fetchUserData };
+    const query = [];
+    if (username) query.push(`login:${username}`);
+    if (location) query.push(`location:${location}`);
+    if (minRepos) query.push(`repos:>${minRepos}`);
+  
+    const url = `https://api.github.com/search/users?q=${query.join('+')}&page=${page}&per_page=10`;
+    const response = await axios.get(url);
+    return response.data;
+  };
+  
+  export { fetchUserData };
